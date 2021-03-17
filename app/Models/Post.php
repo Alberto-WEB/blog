4cd31eb9d91dfc35b7000482d258577ece0d5066
Carlos <alberto.guzman.220793@gmail.com>
2021-03-16 21:02:28 -0600

    blog primera parte terminada

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    use HasFactory;
+
+    //asignacion masica para los que no quieres que se guarden en la DB, y si se utiliza fillable
+    // es para guardar los que quieres que se envien a la DB
+    protected $guarded = ['id', 'create_at', 'update_at'];
+
+    //Relacion uno a muchos inversa
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //Relacion muchos a muchos
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relacion uno a uno poliformica
+    public function image(){
+       return $this->morphOne(Image::class, 'imageable');
+    }
+}
